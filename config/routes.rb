6ca00cfd37e5f6ebb4666999be027658a1f3a67b
rene3255 <ranchoeltecal.com>
2022-02-16 18:10:21 -0600

    routes corrected

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "/chambas#index"
   post '/chamba', to: "chambas#create"
 end
