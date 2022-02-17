e0ef6e748aa81cfa30cdee58eb210c2529a08a6c
rene3255 <ranchoeltecal.com>
2022-02-16 18:26:20 -0600

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to "chambas#index"
+  root to 'chambas#index'
   get 'index', to: 'chambas#index'
 end
