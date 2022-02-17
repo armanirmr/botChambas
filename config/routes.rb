3ac6ba90ba8e8661a886a77ded7f81bc756cc642
rene3255 <ranchoeltecal.com>
2022-02-16 21:14:21 -0600

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'index', to: 'chambas#index'
   # Defines the root path route ("/")
   root to 'chambas#index'
-  get 'index', to: 'chambas#index'
+ 
 end
